242e2fcc9fec8dec613d92ed276c1b81303b1a49
rafli-dio <raflidiomuhammadvalent@gmail.com>
2020-08-18 13:23:59 +0700

    belajar react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './komponen/Navbar';
+import{ BrowserRouter, Route} from "react-router-dom";
+import Dio from "./Dio";
+// 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Route exact path="/">
+      <Navbar />
+    </Route>
+      <Route path="/about">
+      <Navbar />
+      <Dio />
+      </Route>
+    </BrowserRouter>
+  
+  )
 }
 
 export default App;
---
+++ src/Dio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Dio = () => {
+    return <p className="text-primary">Dio</p>
+}
+
+
+
+export default Dio;
